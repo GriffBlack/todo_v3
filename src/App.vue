93669efbe5f2f78cfa13d38353a12eed2808b6cd
GriffBlack <GriffBlack42018@gmail.com>
2022-02-03 01:53:56 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <main>
+    <img alt="Vue logo" src="./assets/logo.png" />
+  </main>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
---
+++ src/components/TInput.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h3></h3>
+        <p></p>
+    </div>
+    <div>
+        <button>Done</button>
+        <button>Remove</button>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
